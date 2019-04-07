0ff2e9a6b4e341a91cc0dbb9d52f5d89935090e4
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-04-07 19:41:08 +0200

    Update Teacher.java

--- src/be/Teacher.java
+++ src/be/Teacher.java
@@ -9,6 +9,54 @@ package be;
  *
  * @author Henrik Ferrari
  */
-public class Teacher implements Person{
+public class Teacher 
+{
+    private String tName;
+    private String tEmail;
+    private int id;
+
+    public Teacher(String tName, String tEmail, int id)
+    {
+        this.tName = tName;
+        this.tEmail = tEmail;
+        this.id = id;
+    }
+
+    public String gettName()
+    {
+        return tName;
+    }
+
+    public void settName(String tName)
+    {
+        this.tName = tName;
+    }
+
+    public String gettEmail()
+    {
+        return tEmail;
+    }
+
+    public void settEmail(String tEmail)
+    {
+        this.tEmail = tEmail;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Teacher{" + "tName=" + tName + ", tEmail=" + tEmail + ", id=" + id + '}';
+    }
+    
     
 }
