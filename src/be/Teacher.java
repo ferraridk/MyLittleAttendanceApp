e88856a641fda1d3d08e0dd0695b66248f641abc
ThaCaper <43811142+ThaCaper@users.noreply.github.com>
2019-04-07 21:16:22 +0200

    Fixed teacher

--- src/be/Teacher.java
+++ src/be/Teacher.java
@@ -6,78 +6,26 @@ import java.util.ArrayList;
  *
  * @author Christian Occhionero
  */
-<<<<<<< HEAD
-public class Teacher 
-{
-    private String tName;
-    private String tEmail;
-    private int id;
-
-    public Teacher(String tName, String tEmail, int id)
-    {
-        this.tName = tName;
-        this.tEmail = tEmail;
-        this.id = id;
-    }
-
-    public String gettName()
-    {
-        return tName;
-    }
-
-    public void settName(String tName)
-    {
-        this.tName = tName;
-    }
-
-    public String gettEmail()
-    {
-        return tEmail;
-    }
-
-    public void settEmail(String tEmail)
-    {
-        this.tEmail = tEmail;
-    }
-
-    public int getId()
-    {
-        return id;
-    }
-
-    public void setId(int id)
-    {
-        this.id = id;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "Teacher{" + "tName=" + tName + ", tEmail=" + tEmail + ", id=" + id + '}';
-    }
-    
-    
-=======
 public class Teacher extends Person
 {
-private ArrayList<SchoolClass> classes;
 
- public Teacher (String name, int id, String email)
+    private ArrayList<SchoolClass> classes;
+
+    public Teacher(String name, int id, String email)
     {
-       super(name,  id, email);
+        super(name, id, email);
         classes = new ArrayList<>();
-        
-    }
- 
- public void addClass(SchoolClass classToAdd)
- {
-     classes.add(classToAdd);
- }
- 
- public ArrayList<SchoolClass> getAllClasses()
- {
-     return classes;
- }
->>>>>>> f7b4c219253bb5d4c30f2c771d73fe95e1173a6a
-}
 
+    }
+
+    public void addClass(SchoolClass classToAdd)
+    {
+        classes.add(classToAdd);
+    }
+
+    public ArrayList<SchoolClass> getAllClasses()
+    {
+        return classes;
+    }
+
+}
